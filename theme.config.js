abb02a5b9c0c231178786f38e44a5c3ebfa8b45e
Justin De Leon <jusmdeleon@gmail.com>
2021-04-05 21:02:24 +0800

    Update footer

--- theme.config.js
+++ theme.config.js
@@ -2,13 +2,13 @@ const YEAR = new Date().getFullYear()
 
 export default {
   footer: (
-    <small style={{ display: 'block', marginTop: '8rem' }}>
-      <time>{YEAR}</time> © Justin De Leon.
+    <small style={{ display: 'flex', justifyContent: 'space-between', marginTop: '8rem' }}>
+      <span>
+        <time>{YEAR}</time> © Justin De Leon.
+        Built with <a href="https://nextra.vercel.app" target="_blank" rel="noopener">Nextra</a>
+      </span>
       <a href="/feed.xml">RSS</a>
       <style jsx>{`
-        a {
-          float: right;
-        }
         @media screen and (max-width: 480px) {
           article {
             padding-top: 2rem;
